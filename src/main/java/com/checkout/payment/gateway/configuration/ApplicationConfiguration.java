edb07f08beee180208af0196e9fbe55e1d82dab8
Ilkin Mehdiyev <46813219+ilkin0@users.noreply.github.com>
2026-02-01 14:04:59 +0400

    feat(init):Refactor to use Java Records and Lombok for model simplification, upgrade dependencies, convert `application.properties` to `application.yml`, and update REST API structure. (#1)

--- src/main/java/com/checkout/payment/gateway/configuration/ApplicationConfiguration.java
+++ src/main/java/com/checkout/payment/gateway/configuration/ApplicationConfiguration.java
@@ -1,19 +1,28 @@
 package com.checkout.payment.gateway.configuration;
 
+import io.netty.channel.ChannelOption;
 import java.time.Duration;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
+@EnableConfigurationProperties(BankSimulatorProperties.class)
 public class ApplicationConfiguration {
 
   @Bean
-  public RestTemplate restTemplate(RestTemplateBuilder builder) {
-    return builder
-        .setConnectTimeout(Duration.ofMillis(10000))
-        .setReadTimeout(Duration.ofMillis(10000))
+  public WebClient bankSimulatorClient(BankSimulatorProperties bankSimulatorProperties) {
+
+    HttpClient httpClient = HttpClient.create()
+        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, bankSimulatorProperties.timeout().connect())
+        .responseTimeout(Duration.ofMillis(bankSimulatorProperties.timeout().read()));
+
+    return WebClient.builder()
+        .baseUrl(bankSimulatorProperties.url())
+        .clientConnector(new ReactorClientHttpConnector(httpClient))
         .build();
   }
 }
--- src/main/java/com/checkout/payment/gateway/exception/CommonExceptionHandler.java
+++ src/main/java/com/checkout/payment/gateway/exception/CommonExceptionHandler.java
@@ -1,21 +1,20 @@
 package com.checkout.payment.gateway.exception;
 
 import com.checkout.payment.gateway.model.ErrorResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class CommonExceptionHandler {
 
-  private static final Logger LOG = LoggerFactory.getLogger(CommonExceptionHandler.class);
 
   @ExceptionHandler(EventProcessingException.class)
   public ResponseEntity<ErrorResponse> handleException(EventProcessingException ex) {
-    LOG.error("Exception happened", ex);
+    log.error("Exception happened", ex);
     return new ResponseEntity<>(new ErrorResponse("Page not found"),
         HttpStatus.NOT_FOUND);
   }
